331590bfccfa3338cff85c897dfe2c1686fb999f
Thomas Hansen <thansen1694@gmail.com>
2017-12-31 16:37:58 -0500

    UsersController

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,17 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages
+    end
+  end
 
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 
 end
