ecb6ef7ebdbd5491e9e6e8d72923983d92d02565
Thomas Hansen <thansen1694@gmail.com>
2017-12-31 16:36:05 -0500

    SessionController complete

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,9 +1,20 @@
 class Api::SessionsController < ApplicationController
   def create
-
+    @user = User.find_by_credentials(params[:email], params[:password])
+    if @user
+      login(@user)
+      render :show
+    else
+      render json: ['The username or password did not match. Please try again'], status: 401
+    end
   end
 
   def destroy
-
+    if current_user.nil?
+      render json: { error: ['Not logged in'], status: 422 }
+    else
+      logout
+      render json: {}
+    end 
   end
 end
