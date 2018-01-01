cd8072a5a807fd3dde366e93cd15e3811f51a18c
Thomas Hansen <thansen1694@gmail.com>
2017-12-31 19:04:07 -0500

    Bootstrapped current user

--- frontend/stromble.jsx
+++ frontend/stromble.jsx
@@ -6,23 +6,19 @@ import { signup, login, logout } from './actions/session/session_actions';
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  // let store;
-  // if (window.currentUser) {
-  //   const preloadedState = { session: { currentUser: window.currentUser } };
-  //   store = configureStore(preloadedState);
-  //
-  //   delete window.currentUser;
-  //
-  // } else {
-  //   store = configureStore;
-  // }
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   window.dispatch = store.dispatch;
   window.getState = store.getState;
   window.signup = signup;
   window.login = login;
   window.logout = logout;
   const root = document.getElementById("root");
-  // ReactDOM.render(<Root store={store} />, root);
   ReactDOM.render(<Root store={store} />, root);
 });
