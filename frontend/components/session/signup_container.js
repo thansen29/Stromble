93fd26b5842fef0ac611ac6c0073485905e8c3d8
Thomas Hansen <thansen1694@gmail.com>
2018-01-02 17:39:13 -0500

    more change for heroku

--- frontend/components/session/signup_container.js
+++ frontend/components/session/signup_container.js
@@ -3,10 +3,6 @@ import SignupForm from './signup_form';
 import { signup } from '../../actions/session/session_actions';
 
 const mapStateToProps = state => {
-<<<<<<< HEAD
-
-=======
->>>>>>> Auth
   return {
     errors: state.errors.session
   };
