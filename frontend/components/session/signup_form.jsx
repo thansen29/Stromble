9450f64c947a8f0a68133e54340c9001c802015e
Thomas Hansen <thansen1694@gmail.com>
2018-01-02 12:12:22 -0500

    Landing nav bar and background image

--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -31,11 +31,36 @@ class SignupForm extends React.Component {
     };
   }
 
+  sampleImage(){
+    const img1 = <img src='http://res.cloudinary.com/stromble/image/upload/v1514660639/pexels-photo-302804_oy0xnz.jpg' />;
+    const img2 = <img src='http://res.cloudinary.com/stromble/image/upload/v1514660638/road-sun-rays-path_t4dtzb.jpg' />;
+    const img3 = <img src='http://res.cloudinary.com/stromble/image/upload/v1514660639/fall-autumn-red-season_bjdaco.jpg' />;
+    const imagesArr = [img1, img2, img3];
+    return imagesArr[Math.floor(Math.random()*imagesArr.length)];
+  }
+
   render(){
+    const image = this.sampleImage();
+
     return (
-      <div>
-        yo
-      </div>
+      <section className="background-container">
+        { image }
+
+        <nav className="landing-nav">
+          <ul className="landing-ul">
+            <li><h1>Here to join?</h1></li>
+            <li><a href="#">Use my email</a></li>
+            <li>
+              <button className="login-button">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                LOG IN
+              </button>
+            </li>
+          </ul>
+        </nav>
+
+
+      </section>
     );
   }
 
