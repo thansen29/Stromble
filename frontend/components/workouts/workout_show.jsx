a01f0bc89abca1364a71effd278938bed70ef2d8
Thomas Hansen <thansen1694@gmail.com>
2018-01-06 11:29:39 -0500

    right side of show styled

--- frontend/components/workouts/workout_show.jsx
+++ frontend/components/workouts/workout_show.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Navbar from '../navbar';
 const WorkoutShow = ({workout}) => {
-
+  //TODO: put actual first name and last name on
+  //parse time and date
   return (
     <section className="background">
       <Navbar />
@@ -25,6 +26,21 @@ const WorkoutShow = ({workout}) => {
             </section>
 
             <section className="show-rightside">
+              <section className="show-stats">
+                <div className="show-statistic">
+                  {workout.distance} {workout.distance_unit.substring(0,2)} <br/>
+                  <span className="stat-text">Distance</span>
+                </div>
+                <div className="show-statistic">
+                  {workout.duration_hr}:{workout.duration_min}:{workout.duration_s} <br/>
+                  <span className="stat-text">Duration</span>
+                </div>
+                <div className="show-statistic">
+                  {workout.elevation} {workout.elevation ? workout.elevation_unit : null}<br/>
+                  {workout.elevation ?
+                    <span className="stat-text">Elevation</span> : null }
+                </div>
+              </section>
 
             </section>
           </section>
