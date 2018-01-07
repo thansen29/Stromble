3d4dbb4353ed902bdb34be52bdaf90b0fe257ffe
Thomas Hansen <thansen1694@gmail.com>
2018-01-07 16:35:46 -0500

    training hover looks pretty good

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -5,24 +5,29 @@ const Navbar = (props) => {
   return (
     <nav className="global-nav">
       <Link to="/dashboard"><div className="logo-nav">stromble</div></Link>
+
       <div className="nav-dropdown">
         <div className="nav-dropdown-content">
           <Link to="/routes">My Routes</Link>
         </div>
-        
-        <div className="nav-dashboard">Dashboard
+
+        <div className="nav-title">Dashboard
           <i className="fa fa-angle-down" aria-hidden="true"></i>
-
-
         </div>
       </div>
 
-      <div className="nav-training">Training
-        <i className="fa fa-angle-down" aria-hidden="true"></i>
-          <div className="nav-dropdown-content">
-            <Link to="/workouts">My Workouts</Link>
-          </div>
+
+      <div className="nav-dropdown">
+        <div className="nav-dropdown-content">
+          <Link to="/workouts">My Workouts</Link>
+        </div>
+
+        <div className="nav-title">Training
+          <i className="fa fa-angle-down" aria-hidden="true"></i>
+        </div>
       </div>
+
+
       <div className="nav-avatar">
         <i className="fa fa-angle-down profile-down" aria-hidden="true"></i>
       </div>
