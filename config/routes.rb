7d6551c3ddb81c00ae5da1ac5beb50bb48691569
Thomas Hansen <thansen1694@gmail.com>
2018-01-08 17:39:37 -0500

    stats ajax calls, fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,26 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
-
+  # TODO: Make sure that nested is right
   namespace :api, defaults: { format: :json } do
     resources :users, only: :create
     resource :session, only: [:create, :destroy]
-    resources :workouts do
-      get :total_run_distance, on: :member
-      get :total_ride_distance, on: :member
-      get :longest_distance, on: :member
-      get :longest_duration, on: :member
-      get :total_runs, on: :member
-      get :total_rides, on: :member
-      get :fasted_speed, on: :member
-    end
+    resources :workouts, except: :new
+    get 'total_run_distance', to: 'workouts#total_run_distance'
+    get 'total_ride_distance', to: 'workouts#total_ride_distance'
+    get 'total_longest_distance', to: 'workouts#total_longest_distance'
+    get 'total_longest_duration', to: 'workouts#total_longest_duration'
+    get 'total_total_runs', to: 'workouts#total_total_runs'
+    get 'total_total_rides', to: 'workouts#total_total_rides'
+    get 'total_fasted_speed', to: 'workouts#total_fasted_speed'
+
+    # resources :workouts do
+    #   get :total_run_distance, on: :member
+    #   get :total_ride_distance, on: :member
+    #   get :longest_distance, on: :member
+    #   get :longest_duration, on: :member
+    #   get :total_runs, on: :member
+    #   get :total_rides, on: :member
+    #   get :fasted_speed, on: :member
+    # end
   end
 end
---
+++ frontend/util/stats_api_util.js
@@ -0,0 +1,48 @@
+export const fetchRunDistance = () => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/total_run_distance'
+  });
+};
+
+export const fetchRideDistance = () => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/total_ride_distance'
+  });
+};
+
+export const fetchLongestDistance = () => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/total_longest_distance'
+  });
+};
+
+export const fetchLongestDuration = () => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/total_longest_duration'
+  });
+};
+
+export const fetchTotalRuns = () => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/total_runs'
+  });
+};
+
+export const fetchTotalRides = () => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/total_rides'
+  });
+};
+
+export const fetchFastedSpeed = () => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/fasted_speed'
+  });
+};
