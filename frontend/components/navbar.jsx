31b858a58f2d279bd1b2232d20fe22ca5e7bf507
Thomas Hansen <thansen1694@gmail.com>
2018-01-07 19:28:13 -0500

    left two on nav bar look better

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -14,47 +14,47 @@ const Navbar = (props) => {
     <nav className="global-nav">
       <Link to="/dashboard"><div className="logo-nav">stromble</div></Link>
 
-      <div className="nav-dropdown">
-        <div className="nav-dropdown-content">
-          <Link to="/routes">My Routes</Link>
-        </div>
-
-        <div className="nav-title">Dashboard
-          <i className="fa fa-angle-down" aria-hidden="true"></i>
-        </div>
-      </div>
-
-      <div className="nav-dropdown">
-        <div className="nav-dropdown-content">
-          <Link to="/workouts">My Workouts</Link>
-        </div>
-
-        <div className="nav-title">Training
-          <i className="fa fa-angle-down" aria-hidden="true"></i>
+      <div className="nav-dashboard-container">
+        <div className="nav-dropdown">
+          <div className="nav-dropdown-content">
+            <Link to="/routes">My Routes</Link>
+          </div>
+          <div className="nav-title">Dashboard
+            <i className="fa fa-angle-down" aria-hidden="true"></i>
+          </div>
         </div>
       </div>
 
+      <div className="nav-training-container">
+        <div className="nav-dropdown">
+          <div className="nav-dropdown-content">
+            <Link to="/workouts">My Workouts</Link>
+          </div>
 
-      <div className="nav-profile-dropdown">
-        <div className="nav-profile-dropdown-content">
-          <Link to="/login" onClick={props.logOut}>Log Out</Link>
-        </div>
-
-        <div className="nav-avatar">
-          <i className="fa fa-angle-down profile-down nav-title" aria-hidden="true"></i>
+          <div className="nav-title">Training
+            <i className="fa fa-angle-down" aria-hidden="true"></i>
+          </div>
         </div>
       </div>
 
+        <div className="nav-profile-dropdown">
+          <div className="nav-profile-dropdown-content">
+            <Link to="/login" onClick={props.logOut}>Log Out</Link>
+          </div>
 
-      <div className="nav-upload-dropdown">
-        <div className="nav-upload-dropdown-content">
-          <Link to="/workouts/new">Create a workout</Link>
-          <Link to="/routes/new">Create a route</Link>
+          <div className="nav-avatar">
+            <i className="fa fa-angle-down profile-down nav-title" aria-hidden="true"></i>
+          </div>
         </div>
 
-        <div className="nav-upload-button"></div>
+        <div className="nav-upload-dropdown">
+          <div className="nav-upload-dropdown-content">
+            <Link to="/workouts/new">Create a workout</Link>
+            <Link to="/routes/new">Create a route</Link>
+          </div>
 
-      </div>
+          <div className="nav-upload-button"></div>
+        </div>
     </nav>
   );
 };
