1b8d0edb9c7405c237acb2b82418a380b30c3a3a
Thomas Hansen <thansen1694@gmail.com>
2018-01-09 15:01:21 -0500

    jbuilder for routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :password, length: { minimum: 5, allow_nil: true }
 
-  # has_many :routes
   has_many :workouts
+  has_many :routes
 
   attr_reader :password
   after_initialize :ensure_session_token
---
+++ app/views/api/routes/_routes.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! route, :id, :user_id, :title, :start_lat, :start_lng, :end_lat,
+:end_lng, :distance, :distance_unit, :elevation_gain, :elevation_unit, :private
