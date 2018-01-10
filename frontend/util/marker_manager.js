7689cadd8fcb9999a40e5ae1fb80a8324c5eef91
Thomas Hansen <thansen1694@gmail.com>
2018-01-10 10:56:29 -0500

    marker appears on map at initial position

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -9,7 +9,17 @@ class MarkerManager {
   updateMarkers(coords){
     console.log("lat" + coords.lat);
     console.log("lng" + coords.lng);
-    console.log("Updating endpoint");
+    // console.log("Updating endpoint");
+
+    const marker = new google.maps.Marker({
+      position: coords,
+      map: this.map
+    });
+    // console.log(marker);
+    // debugger
+    //marker.position.lat()
+
+
   }
 }
 
