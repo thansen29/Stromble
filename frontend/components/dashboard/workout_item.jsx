1f72bd6db6271b2312933b94b62e05a734f83a44
Thomas Hansen <thansen1694@gmail.com>
2018-01-10 13:35:27 -0500

    fix for heroku

--- frontend/components/dashboard/workout_item.jsx
+++ frontend/components/dashboard/workout_item.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 
-// const momentDurationFormatSetup =
-require("moment-duration-format");
-
 
 class WorkoutItem extends React.Component {
   constructor(props){
