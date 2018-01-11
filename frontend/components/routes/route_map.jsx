8e8e44fa29e136e17db1994322055377f7113f89
Thomas Hansen <thansen1694@gmail.com>
2018-01-10 20:37:40 -0500

    everything still works without the marker manager

--- frontend/components/routes/route_map.jsx
+++ frontend/components/routes/route_map.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import MarkerManager from '../../util/marker_manager';
+// import MarkerManager from '../../util/marker_manager';
 
 class RouteMap extends React.Component {
   constructor(props){
     super(props);
+    this.markers = {};
+    this.map;
+    this.state = {
+      startLat: '',
+      startLng: '',
+      endLat: '',
+      endLng: '',
+      distance: '',
+      elevation: ''
+    };
+
+    this.id = 1;
+    this.routePath = [];
+    this.elevations = [];
   }
 
   componentDidMount(){
@@ -14,15 +28,108 @@ class RouteMap extends React.Component {
     };
 
     this.map = new google.maps.Map(this.mapNode, mapOptions);
-    this.MarkerManager = new MarkerManager(this.map);
+    // this.MarkerManager = new MarkerManager(this.map);
+
 
     this.map.addListener("click", (e) => {
       const lat = e.latLng.lat();
       const lng = e.latLng.lng();
       const position = { lat, lng };
-      this.MarkerManager.createMarker(position);
+      this.createMarker(position);
 
     });
+  }
+
+  createMarker(coords){
+    const marker = new google.maps.Marker({
+      position: coords,
+      map: this.map,
+      id: this.id
+    });
+    this.markers[this.id] = marker;
+    this.id++;
+    marker.addListener("click", () => {
+      this.removeMarker(marker.id);
+    });
+    // console.log("marker created");
+    if(Object.values(this.markers).length === 2){
+      this.createRoute();
+    }
+  }
+
+  removeMarker(markerId){
+    this.markers[markerId].setMap(null);
+    delete this.markers[markerId];
+  }
+
+  createRoute(){
+    const start = this.markers[1].getPosition();
+    const end = this.markers[2].getPosition();
+    const routeRunRequest = {
+      origin: start,
+      destination: end,
+      travelMode: 'WALKING',
+    };
+
+    // const routeRideRequest = {
+    //   origin: start,
+    //   destination: end,
+    //   travelMode: 'BICYCLING',
+    // };
+
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(this.map);
+
+    directionsService.route(routeRunRequest, ((directionsResult, directionsStatus) => {
+      if(directionsStatus === "OK"){
+        directionsRenderer.setDirections(directionsResult);
+        let distance = directionsResult.routes[0].legs[0].distance.text;
+        this.routePath = directionsResult.routes[0].overview_path;
+
+          const elevationService = new google.maps.ElevationService();
+          elevationService.getElevationAlongPath({
+            path: this.routePath,
+            samples: this.routePath.length
+          }, ((elevationResult, elevationStatus) => {
+              if(elevationStatus === "OK"){
+                elevationResult.forEach((sample) => {
+                  this.elevations.push(sample.elevation);
+                });
+                const max = this.elevations.sort()[this.elevations.length-1];
+                const min = this.elevations.sort()[0];
+                let elevation = max - min;
+                this.storeData(distance, start, end, elevation);
+              }
+          }).bind(this));
+
+
+      }
+    }).bind(this));
+  }
+
+  storeData(distance, start, end, elevation){
+    if(distance){
+      this.setState({
+        startLat: start.lat(),
+        startLng: start.lng(),
+        endLat: end.lat(),
+        endLng: end.lng(),
+        distance: parseFloat(distance),
+        elevation: parseInt(elevation)
+      });
+      // this.startLat = start.lat();
+      // this.startLng = start.lng();
+      // this.endLat = end.lat();
+      // this.endLng = end.lng();
+      // this.distance = parseFloat(distance);
+      // this.elevation = parseInt(elevation);
+      // const data = {
+      //   startLat, startLng, endLat, endLng, distance, elevation
+      // };
+      // return data;
+    }
+    debugger
   }
 
 
