0715a6ee75d1214773c624c6216661d6bfa8b074
Thomas Hansen <thansen1694@gmail.com>
2018-01-11 17:12:41 -0500

    all routes looks pretty good

--- frontend/components/routes/route_item.jsx
+++ frontend/components/routes/route_item.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const RouteItem = ({route}) => {
-  debugger
   const startLat = route.start_lat;
   const startLng = route.start_lng;
   const endLat = route.end_lat;
@@ -10,7 +10,27 @@ const RouteItem = ({route}) => {
   const url = `https://maps.googleapis.com/maps/api/staticmap?&size=300x300&path=color:0x0000ff|weight:5|${startLat},${startLng}|${endLat},${endLng}`;
   return (
     <li>
-      <img src={url} alt="preview goes here"/>
+      <div className="route-item-container">
+        <img src={url} alt="preview goes here"/>
+        <div className="route-item-details">
+          <Link to={`/routes/${route.id}`}>{route.title}</Link>
+          <ul className="route-item-content">
+            <li>
+              <span className="route-item-data">{route.distance}</span>
+                <span className="show-datetime unit">mi</span>
+              <span className="route-item-data">{route.elevation_gain}</span>
+                <span className="show-datetime unit">meters</span>
+            </li>
+
+
+            <li className="item-stat">
+              <span className="route-item-data-unit">Distance</span>
+                <span className="route-item-data-unit">Elevation Gain</span>
+            </li>
+
+          </ul>
+        </div>
+      </div>
     </li>
   );
 };
