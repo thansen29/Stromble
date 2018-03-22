10ca78f7a298b61a6ed1adb45a8160901f6a7c4e
Thomas Hansen <thansen1694@gmail.com>
2018-03-22 13:36:13 -0400

    Update search to recognize substrings

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -7,10 +7,12 @@ class Api::SearchController < ApplicationController
       if name.length > 1 #searched with two names
         fname = name[0].downcase
         lname = name[1].downcase
-        @users = User.where('lower(fname) = ? AND lower(lname) = ?', fname, lname)#
+        @users = User.where('lower(fname) LIKE ? AND lower(lname) LIKE ?', "%#{fname}%", "%#{lname}%")
+        # @users = User.where('lower(fname) = ? AND lower(lname) = ?', fname, lname)
       else
         name = name[0].downcase
-        @users = User.where('lower(fname) = ? OR lower(lname) = ?', name, name)#
+        @users = User.where('lower(fname) LIKE ? OR lower(lname) LIKE ?', "%#{name}%", "%#{name}%")
+        # @users = User.where('lower(fname) = ? OR lower(lname) = ?', name, name)#
       end
 
 
