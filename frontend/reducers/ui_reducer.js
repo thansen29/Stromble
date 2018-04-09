f73ce08555ce88e3a107442ac01644b8616e3088
Thomas Hansen <thansen1694@gmail.com>
2018-04-09 16:23:47 -0400

    Remove dropdown reducer - old code

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import modalReducer from './modal_reducer';
-import dropdownReducer from './dropdown_reducer';
 import searchReducer from './search_reducer';
 
 const uiReducer = combineReducers({
   modal: modalReducer,
-  dropdown: dropdownReducer,
   search: searchReducer
 });
 
